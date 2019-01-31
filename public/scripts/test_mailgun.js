1b9687ac2fa31ec69442626a3d4c00040fa1c3cb
sumedha <sumedhanarayanan@gmail.com>
2019-01-31 23:12:09 +0000

    Add a test for the mailgun api using npm package for mailgun.

---
+++ public/scripts/test_mailgun.js
@@ -0,0 +1,15 @@
+var api_key = 'db88c9e27666194d9870bc47555b257e-c8c889c9-78662c6b';
+var domain = 'sandbox15da19073efc4253b56be0368fe36362.mailgun.org';
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+
+var data = {
+  from: 'admin@sandbox15da19073efc4253b56be0368fe36362.mailgun.org',
+  to: 'sumedhanarayanan@gmail.com',
+  subject: 'Hello',
+  text: 'Testing some Mailgun awesomeness! This is pretty cool! I think :)'
+};
+
+mailgun.messages().send(data, function (error, body) {
+  if (error) console.log(error);
+  console.log(body);
+});
