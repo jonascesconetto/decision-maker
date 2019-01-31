24f870ea093b09c91c89d80b8b6660c9b35ba8e6
sumedha <sumedhanarayanan@gmail.com>
2019-01-31 02:24:38 +0000

    Migrate to voters table

---
+++ db/migrations/20190131021847_create_voterstable.js
@@ -0,0 +1,13 @@
+//  Creates voters table schema
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('voters', function (table) {
+    table.increments('id');
+    table.integer('polls_id').references('polls.id');
+    table.string('first_name').notNullable();
+    table.string('last_name').notNullable();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('voters');
+};
