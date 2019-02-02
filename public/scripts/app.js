227b460fe4034458aa4f811a704f61888bf47f55
pnolan89 <petenol@gmail.com>
2019-02-02 01:26:31 +0000

    add error message catch to vote page when user does not enter name

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,7 @@
 // USE FOR ON PAGE EVENTS ONLY
 $(() => {
   if (document.getElementById('sortable')) {
+    $("#errorMsg").hide();
     let orderArray = [];
     $( "#sortable" ).sortable({
     update: function(event, ui) {
@@ -11,21 +12,35 @@ $(() => {
     });
     $( "#sortable" ).disableSelection();
 
-    $( "#voteSubmit").on("click", function() {
-      $.ajax({
-        method: 'POST',
-        url: '/polls/:v_url/',
-        data: JSON.stringify({orderArray}),
-      contentType: 'application/json',
-      success:function(result){
-        console.log("we are working");
-        window.location = "http://localhost:8080" ;
-      },
-      error:function(err){
-        console.log("something happened");
+    $( "#voteSubmit").on("click", function(event) {
+      event.preventDefault();
+      let voterName = $(this).parents("#voteSubmitForm").find("input").val();
+      if(voterName === "") {
+        $("#errorMsg").slideDown();
+      } else {
+        $("#errorMsg").slideUp();
+        $.ajax({
+          method: 'POST',
+          url: '/polls/:v_url/',
+          data: JSON.stringify({orderArray}),
+          contentType: 'application/json',
+          success:function(result){
+            console.log("we are working");
+            window.location = "http://localhost:8080" ;
+          },
+          error:function(err){
+            console.log("something happened");
+          }
+        });
+        console.log(orderArray);
       }
-      });
-      console.log(orderArray);
+    });
+  }
+
+  if (document.getElementById('addOptionBtn')) {
+    let optionCount = 3;
+    $( "#addOptionBtn").on('click', function () {
+      optionCount += 1;
     });
   }
 });
