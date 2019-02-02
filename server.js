de7536bada9ba9e5177556af14380f48ef34f0a3
pnolan89 <petenol@gmail.com>
2019-02-02 18:54:53 +0000

    init commit

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.post('/polls/:url', (req, res) => {
     .leftJoin('polls', 'polls.id', 'candidates.polls_id')
     .where('vote_url', voteURL)
     .then((results) => {
-      // console.log('POST vote', vote);
+      console.log('POST vote', vote);
       writeVotes(vote, results[0].polls_id, req.body.voterName);
       return borda(vote);
     })
