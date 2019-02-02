31af6de44e2f86b9f752ea7ffaec00dedc8740ee
sumedha <sumedhanarayanan@gmail.com>
2019-02-02 03:44:06 +0000

    update seed file and edits to admin helpers

--- test_mailgun.js
+++ test_mailgun.js
@@ -1,10 +1,12 @@
-var api_key = 'db88c9e27666194d9870bc47555b257e-c8c889c9-78662c6b';
-var domain = 'sandbox15da19073efc4253b56be0368fe36362.mailgun.org';
+
+require('dotenv').config();
+const api_key = process.env.MAILGUN_KEY;
+var domain = 'sandbox090311b5c2024041bfa7f700785a0ec5.mailgun.org';
 var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 
 var data = {
-  from: 'admin@sandbox15da19073efc4253b56be0368fe36362.mailgun.org',
-  to: 'petenol@gmail.com',
+  from: 'admin@sandbox090311b5c2024041bfa7f700785a0ec5.mailgun.org',
+  to: 'sumedha.lhl@gmail.com',
   subject: 'Hello',
   text: 'Testing some Mailgun awesomeness! This is pretty cool! I think :)'
 };
@@ -13,3 +15,5 @@ mailgun.messages().send(data, function (error, body) {
   if (error) console.log(error);
   console.log(body);
 });
+
+console.log(api_key);
