d9a24fb51bb51d0f6669c4c0b15943fd70b4b8ad
sumedha <sumedhanarayanan@gmail.com>
2019-02-03 16:45:56 +0000

    lint adminquery_helper

--- adminquery_helper.js
+++ adminquery_helper.js
@@ -4,32 +4,26 @@ const ENV         = process.env.ENV || 'development';
 const knexConfig  = require('./knexfile');
 const knex        = require('knex')(knexConfig[ENV]);
 
-// Query function to help render the admin page displaying who votes what
+// Query function to help render the admin page displaying voters' and their preferences
 
-// query to find all the voters in a certain poll
-
-
+// query to find all the distinct voters in a certain poll
 function getUsers(polls_id) {
-
   return knex('votes')
-  .distinct('username')
-  .select('username')
-  .where('polls_id', polls_id)
-  .then( (rows) => {
-    var users = rows.map((element) => {return element.username;});
-    return users;
-  });
+    .distinct('username')
+    .select('username')
+    .where('polls_id', polls_id)
+    .then((rows) => {
+      var users = rows.map((element) => {return element.username;});
+      return users;
+    });
 }
-//usage:
-// getUsersFromDB(polls_id).then( (result) => {
-  // templateVars.voters = results;
-// })
+//   export function that reads the database for the preferences of all distinct voters in a poll
 module.exports = function getUsersFromDB(polls_id) {
   console.log('pollsid', polls_id);
   return getUsers(polls_id)
-    .then( (users) => {
+    .then((users) => {
       console.log('users', users);
-      var promiseArr = users.map( (elem) => {
+      let promiseArr = users.map((elem) => {
         return knex
           .select('candidate')
           .from('votes')
@@ -38,29 +32,25 @@ module.exports = function getUsersFromDB(polls_id) {
             'username': elem,
             'votes.polls_id': polls_id})
           .orderBy('rating', 'desc')
-          .then( (rows) => {
-            var arr = rows.map( (elem) => {
+          .then((rows) => {
+            let arr = rows.map((elem) => {
               return elem.candidate;
-            })
-
+            });
+            //  Returns obj { votername: 'option1,option2, etc.}
             let obj = {
               username: elem,
               ranking: arr.join(',')
-            }
+            };
             return obj;
-          })
-      })
+          });
+      });
       return Promise.all(promiseArr).then( (result) => {
         return (result);
-      })
+      });
     })
-    .then( (result) => {
+    .then((result) => {
       console.log (result);
       return result;
-
       // pass here to admin.ejs and catch error in server.js file
-    })
-}
-
-
-// console.getUsersFromDB(polls_id);
+    });
+};
